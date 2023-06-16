8bd03354288302c77a903a2ca95fa574e869dd29
unknown <thahira.f2021@gmail.com>
2023-06-16 17:19:03 -0400

    first commit

---
+++ dom_practise/app.js
@@ -0,0 +1,9 @@
+//  let items =doument.querySelector('.menu-item')
+//  console.log(items);
+//  items[0].style.color = 'orange';
+let paragraph = doucument.createElement('p');
+paragraph.textContent = "Hey i'm Red";
+doument.body.appendChild(paragraph);
+let header = document.createElement('h3');
+header.textContent = "I'm blue";
+doument.body.appenChild(header);
